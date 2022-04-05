2f2120b3182b44239faed0ce5b9e76eebb0d01e5
Linh Ly <60450677+TsmKaori@users.noreply.github.com>
2022-04-05 11:00:55 -0500

    Add wave UI

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 [System.Serializable]
 public class Wave
@@ -22,9 +24,19 @@ public class WaveController : MonoBehaviour
 
 
     private bool canSpawn = true;   //This indicate if can spawn. Turn off if dont want to spawn intitally
+    private bool canAnimate = false;
+    private bool currentlyCountdown;
     private float nextSpawnT;
 
     public AbilitySystem abilitySystem;
+    public Animator animator;
+
+    public TMP_Text waveCompleteText;
+    public TMP_Text countdownBuffer;
+    public float timeRemaining = 32;
+
+
+    public 
     // Start is called before the first frame update
     void Start()
     {
@@ -38,32 +50,55 @@ public class WaveController : MonoBehaviour
         SpawnWave();
 
         GameObject[] totEnemies = GameObject.FindGameObjectsWithTag("Enemy");
-        if(totEnemies.Length == 0 && !canSpawn && currWaveNum+1 != waves.Length)
+        if(totEnemies.Length == 0 && canAnimate && currWaveNum+1 != waves.Length)
         {
-            currWaveNum++;
-            if (currWaveNum == 1)        //Add ability or whatever else need to be happen after whatever wave here
-            {
-                abilitySystem.unlockAbilties(AbilitySystem.SkillType.inferno);
-            }
-            else if (currWaveNum == 2)
-            {
-                abilitySystem.unlockAbilties(AbilitySystem.SkillType.ringOfFire);
-            }
-            else if (currWaveNum == 3)
-            {
-                abilitySystem.unlockAbilties(AbilitySystem.SkillType.grappling);
-            }
-            else if (currWaveNum == 4)
-            {
-                abilitySystem.unlockAbilties(AbilitySystem.SkillType.rapidFire);
-            }
-            else if (currWaveNum == 5)
-            {
-
-            }
-            //Add wave interval here. This area can buffer until the next call
-            canSpawn = true;
+            animator.SetTrigger("WaveComplete");
+            canAnimate = false;
+            currentlyCountdown = true;
+            //spawnNext();
         }
+
+        if (currentlyCountdown)
+        {
+            timeRemaining -= Time.deltaTime;
+            int seconds = (int)timeRemaining;
+            //Debug.Log(seconds);
+            countdownBuffer.text = seconds.ToString();
+            if (timeRemaining <= 0)
+            {
+                currentlyCountdown = false;
+                timeRemaining = 32;
+                animator.SetTrigger("BufferComplete");
+                //spawnNext();
+            }
+        }
+    }
+
+    void spawnNext()
+    {
+        currWaveNum++;
+        if (currWaveNum == 1)        //Add ability or whatever else need to be happen after whatever wave here
+        {
+            abilitySystem.unlockAbilties(AbilitySystem.SkillType.inferno);
+        }
+        else if (currWaveNum == 2)
+        {
+            abilitySystem.unlockAbilties(AbilitySystem.SkillType.ringOfFire);
+        }
+        else if (currWaveNum == 3)
+        {
+            abilitySystem.unlockAbilties(AbilitySystem.SkillType.grappling);
+        }
+        else if (currWaveNum == 4)
+        {
+            abilitySystem.unlockAbilties(AbilitySystem.SkillType.rapidFire);
+        }
+        else if (currWaveNum == 5)
+        {
+
+        }
+        //Add wave interval here. This area can buffer until the next call
+        canSpawn = true;
     }
 
     void SpawnWave()
@@ -80,6 +115,7 @@ public class WaveController : MonoBehaviour
             if(currWave.numOfEnemies == 0)
             {
                 canSpawn = false;
+                canAnimate = true;
             }
         }
     }
